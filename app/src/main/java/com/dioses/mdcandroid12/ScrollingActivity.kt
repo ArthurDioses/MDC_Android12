0275be8713241e9d1c1ae97e21ae9a2051c9ef14
adiosesr <Peru123456>
2022-03-14 21:42:16 -0500

    - MDC: Snackbar and Toast

--- app/src/main/java/com/dioses/mdcandroid12/ScrollingActivity.kt
+++ app/src/main/java/com/dioses/mdcandroid12/ScrollingActivity.kt
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.dioses.mdcandroid12.databinding.ActivityScrollingBinding
 import com.google.android.material.bottomappbar.BottomAppBar
 
@@ -36,6 +37,14 @@ class ScrollingActivity : AppCompatActivity() {
         }
         binding.content.btnSkip.setOnClickListener {
             binding.content.cvAdd.visibility = View.GONE
+        }
+        binding.content.btnBuy.setOnClickListener {
+            Snackbar.make(it, R.string.card_buying, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .setAction(R.string.card_to_go, {
+                    Toast.makeText(this, R.string.card_historial, Toast.LENGTH_SHORT).show()
+                })
+                .show()
         }
     }
 
